19c721de4797f16c378baec3bd9ba4e6afb7bfa1
Rincon23 <enzorincon2003@gmail.com>
2025-08-18 21:13:07 -0300

    Aula 2 Framework

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express")
-const app = express
+const app = express()
 const port = 3000  
 
-app.request(express.json())
+app.use(express.json())
+
+let alunos = [
+    { id: 1, nome: "João"}
+]
+
+app.get('/aluno', (req, res) => {
+    res.json(alunos)
+})
+
+app.post('/aluno', (req, res) => {
+    const novoAluno = { id: alunos.length + 1, ...req.body}
+    alunos.push(novoAluno)
+    res.status(201).json(novoAluno)
+})
 
 app.listen(port,() => {
     console.log("Servidor de API funcionando")
