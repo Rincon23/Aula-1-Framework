8b864c5ece1d25813bb10df05655986ebb9c3a83
Rincon23 <enzorincon2003@gmail.com>
2025-08-25 20:38:00 -0300

    Aula 2

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const swaggerUi = require("swagger-ui-express")
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerOptions = require("./extend/swagger")
+
+
+const app = express()
+const port = 3000  
+
+const specs = swaggerJsdoc(swaggerOptions)
+
+app.use(express.json())
+
+let alunos = [
+    { id: 1, nome: "João"}
+]
+
+app.get('/aluno', (req, res) => {
+    res.json(alunos)
+})
+
+app.post('/aluno', (req, res) => {
+    const novoAluno = { id: alunos.length + 1, ...req.body}
+    alunos.push(novoAluno)
+    res.status(201).json(novoAluno)
+})
+
+app.put('/aluno/:id',(req,res) => {
+    const {id} = req.params
+    const alunoIndex = alunos.findIndex(a => a.id == id)
+
+    if (alunoIndex > -1) {
+        alunos[alunoIndex] = {id: Number(id), ...req.body}
+        res.status(200).json(alunos[alunoIndex])
+    } else {
+        res.status(404).json({message:"Aluno não encontrado"})
+    }
+})
+
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(specs))
+
+app.listen(port,() => {
+    console.log("Servidor de API funcionando")
+})
